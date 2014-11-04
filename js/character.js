bf044f2cf66d20b1f5855708cd1c7c6234a26ab2
Bruno Bord <bruno@jehaisleprintemps.net>
2014-11-04 22:55:35 +0100

    character sheet, first shot

---
+++ js/character.js
@@ -0,0 +1,36 @@
+$(document).ready(function() {
+
+    function progressBarUpdate(bar, input, maximum) {
+        var width = $(input).val() / maximum * 100.0;
+        var barClass = 'progress-bar-danger';
+        if (width > 66.0) {
+            barClass = 'progress-bar-success';
+        } else if (width > 33.0) {
+            barClass = 'progress-bar-warning';
+        }
+        $(bar).css({width: width + '%'}).attr('class', '').addClass('progress-bar ' + barClass);
+    }
+
+    var querystring = location.search.slice(1);
+    var vars = $.deserialize(querystring);
+    var re = /\+/gi;
+    for (key in vars) {
+        var value = vars[key];
+        var input = $('input[name='+key+']');
+        if (input.attr('type') == 'text') {
+            input.val(value.replace(re, " "));
+        } else {
+            input.val(value);
+        }
+    };
+    progressBarUpdate('#progressbar-network', 'input[name=gauge-network]', 6.0);
+
+    $('form input').change(function() {
+        location.search = $('form input').serialize();
+    });
+
+    $('input[name=gauge-network]').change(function() {
+        progressBarUpdate('#progressbar-network', 'input[name=gauge-network]', 6.0);
+    });
+
+})
